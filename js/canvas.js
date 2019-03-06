11b7dc9e8b28ab9d0398c59abb4848dff1e3712a
deptster <deepantripathi0000@gmail.com>
2019-03-07 01:21:13 +0530

    update

---
+++ js/canvas.js
@@ -0,0 +1,39 @@
+//rainy day code
+	var nbDrop = 100;
+
+		function randRange(minNum, maxNum) {
+			return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+		}
+
+		function createRain() {
+			for (i = 1; i < nbDrop; i++){
+				var dropLeft = randRange(0, 1600);
+				var dropTop = randRange(-1000, 1400);
+
+				$('.rain').append('<div class="drop" id="drop' + i + '"></div>');
+				$('#drop'+ i).css('left', dropLeft);
+				$('#drop'+ i).css('top', dropTop);
+			}
+		}
+
+
+//move Box animation on index page
+	
+//put code here
+
+
+
+//canvas file design 
+
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var context = canvas.getContext('2d');
+
+context.fillRect(100, 100, 20, 100);
+context.fillRect(200, 300, 20, 100);
+context.fillRect(300, 200, 20, 100);
+
+console.log(canvas);
